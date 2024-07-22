7009e12f96adbb637c42020c5fc4ee2eac40836e
ODG <o.d.g@kakao.com>
2024-07-22 21:31:51 +0900

    Feat: GlobalException 예외 처리 추가

---
+++ src/main/java/com/dtbid/dropthebid/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.dtbid.dropthebid.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorCode {
+    // 에러 코드 작성 ex) ErrorCode("MESSAGE")
+
+    ;
+
+
+    private String MESSAGE;
+}
