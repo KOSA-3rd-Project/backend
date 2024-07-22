8364270eef4c541460d7f8d1019ee144fdba17ca
OhDongGeon <o.d.g@kakao.com>
2024-07-22 12:29:04 +0900

    init

---
+++ src/main/java/com/dtbid/dropthebid/DropthebidApplication.java
@@ -0,0 +1,13 @@
+package com.dtbid.dropthebid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropthebidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropthebidApplication.class, args);
+    }
+
+}
