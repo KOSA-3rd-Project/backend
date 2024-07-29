70d05cdcb1e4e700282cbf4b2aba0cb536ab358d
OhDongGeon <o.d.g@kakao.com>
2024-07-29 12:31:00 +0900

    Change_jwt_logic
    Change_jwt_logic : JWT 로직 추가 및 수정

--- src/main/java/com/dtbid/dropthebid/member/controller/MemberController.java
+++ src/main/java/com/dtbid/dropthebid/member/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.dtbid.dropthebid.member.controller;
 import com.dtbid.dropthebid.member.model.form.SignInForm;
 import com.dtbid.dropthebid.member.model.form.SignUpForm;
 import com.dtbid.dropthebid.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +21,28 @@ public class MemberController {
 
   private final MemberService memberService;
 
+
   // 오동건 - 회원가입
   @PostMapping("/signup")
   public ResponseEntity<String> memberSignUp(@RequestBody @Valid SignUpForm signUpForm) {
 
-    String signUp = memberService.memberSignUp(signUpForm);
-
-    return new ResponseEntity<>(signUp, HttpStatus.CREATED);
+    return new ResponseEntity<>(memberService.memberSignUp(signUpForm), HttpStatus.CREATED);
   }
 
   // 오동건 - 로그인
   @PostMapping("/signin")
-  public ResponseEntity<String> memberSignIn(@RequestBody @Valid SignInForm signInForm)
-      throws InterruptedException {
+  public ResponseEntity<String> memberSignIn(@RequestBody @Valid SignInForm signInForm,
+      HttpServletResponse response) {
 
-    return new ResponseEntity<>(memberService.memberSignIn(signInForm), HttpStatus.CREATED);
+    return new ResponseEntity<>(memberService.memberSignIn(signInForm, response),
+        HttpStatus.CREATED);
+  }
+
+  // 오동건 - refresh token 확인
+  @PostMapping("/checks/refresh-token")
+  public ResponseEntity<String> checkRefreshToken(
+      @CookieValue(value = "refreshToken", required = false) String refreshToken) {
+
+    return new ResponseEntity<>(memberService.checkRefreshToken(refreshToken), HttpStatus.CREATED);
   }
 }
---
+++ src/main/java/com/dtbid/dropthebid/security/component/JwtAuthenticationFilter.java
@@ -0,0 +1,74 @@
+package com.dtbid.dropthebid.security.component;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  public final String TOKEN_HEADER = "Authorization";
+  public final String TOKEN_PREFIX = "Bearer ";
+
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) {
+
+    String[] api = {"/members/signin", "/members/signup", "/auctions/month", "/auctions/popular",
+        "/auctions/new", "/search", "/ws", "/auctions/month", "/auctions/popular",
+        "/auctions/new", "/search","/members/checks/refresh-token"};
+
+    String path = request.getRequestURI();
+
+    return Arrays.stream(api).anyMatch(path::startsWith);
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    try {
+      // 토큰
+      String token = request.getHeader(TOKEN_HEADER);
+
+      // 토큰이 없을 경우
+      if (!StringUtils.hasText(token)) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        return;
+      }
+
+      token = token.substring(TOKEN_PREFIX.length());
+
+      if (jwtTokenProvider.validateToken(token, true)) {
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(token, true);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    } catch (ExpiredJwtException ex) {
+      // 만료 에러 엑세스 토큰 확인 필요
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      return;
+    } catch (JwtException ex) {
+      // 토큰 변형 에러
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      return;
+    }
+    filterChain.doFilter(request, response);
+  }
+}
