90721e1c391fd7d6ebb6c2a88e09b0c713aed3db
JaeeunCho <redjoun@gmail.com>
2024-07-30 16:02:53 +0900

    Feat chatroom

---
+++ src/main/java/com/dtbid/dropthebid/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,12 @@
+package com.dtbid.dropthebid.chat.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.dtbid.dropthebid.chat.model.ChatRoom;
+
+@Mapper
+public interface ChatRoomRepository {
+
+    void insertChatRoom(ChatRoom chatRoom);
+    
+    Integer getChatRoomId(Integer auctionId, Integer memberId);
+}
